56edab0acc58c21bdfd0d7b40fd8d2040726982a
Alain Schaller <SchallerAla@gmail.com>
2016-01-26 13:10:06 +0200

    Correct show method for the samplings: - integrate the product link with the results - handle "today" or "yesterday" parameters - if no _time_ parameters were given, just filter for todays results

--- webserver/app/Http/Controllers/SamplingsController.php
+++ webserver/app/Http/Controllers/SamplingsController.php
@@ -34,28 +34,51 @@ class SamplingsController extends Controller
      */
     public function show($productId, ShowingSamplingsRequest $request)
     {
-        $product = Product::findOrFail($productId);
+        Product::findOrFail($productId); // Just to throw a failure if the product is not found
         $samplingsQueryBuilder = Sampling::query();
+
+        $dateFilter = false;
+
+        $samplingsQueryBuilder->join('sensors', 'sensor_id', '=', 'sensors.id');
+        $samplingsQueryBuilder->join('products', 'sensors.product_id', '=', 'products.id');
+        $samplingsQueryBuilder->where('products.id', $productId);
 
         $limit = $request->has('limit') && $request->limit < 1000 ? $request->limit : 1000;
 
         if ($request->date)
         {
             $samplingsQueryBuilder->whereDate('samplings.created_at', '=', $request->date);
+            $dateFilter = true;
+        }
+        elseif ($request->exists("today"))
+        {
+            $samplingsQueryBuilder->whereDate('samplings.created_at', '=', Carbon::today()->toDateString());
+            $dateFilter = true;
+        }
+        elseif ($request->exists("yesterday"))
+        {
+            $samplingsQueryBuilder->whereDate('samplings.created_at', '=', Carbon::today()->subDay()->toDateString());
+            $dateFilter = true;
         }
         elseif ($request->start)
         {
             $samplingsQueryBuilder->whereDate('samplings.created_at', '>=', $request->start);
+            $dateFilter = true;
 
             if ($request->end)
             {
                 $samplingsQueryBuilder->whereDate('samplings.created_at', '<=', $request->end);
+                $dateFilter = true;
             }
+        }
+
+        if ( ! $dateFilter)
+        {
+            $samplingsQueryBuilder->whereDate('samplings.created_at', '=', Carbon::today()->toDateString());
         }
 
         if ($request->generic_sensor_id)
         {
-            $samplingsQueryBuilder->join('sensors', 'sensor_id', '=', 'sensors.id');
             $samplingsQueryBuilder->join('generic_sensors', 'sensors.generic_sensor_id', '=', 'generic_sensors.id');
             $samplingsQueryBuilder->where('generic_sensors.id', $request->generic_sensor_id);
         }
