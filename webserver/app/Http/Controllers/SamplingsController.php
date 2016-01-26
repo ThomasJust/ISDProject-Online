0cb975479fe567bd639d3ed414c8aad192e2e804
Alain Schaller <SchallerAla@gmail.com>
2016-01-26 13:22:46 +0200

    refactor 56edab0

--- webserver/app/Http/Controllers/SamplingsController.php
+++ webserver/app/Http/Controllers/SamplingsController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\ShowingSamplingsRequest;
 use App\Product;
 use App\Sampling;
 use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -37,42 +38,16 @@ class SamplingsController extends Controller
         Product::findOrFail($productId); // Just to throw a failure if the product is not found
         $samplingsQueryBuilder = Sampling::query();
 
-        $dateFilter = false;
-
         $samplingsQueryBuilder->join('sensors', 'sensor_id', '=', 'sensors.id');
         $samplingsQueryBuilder->join('products', 'sensors.product_id', '=', 'products.id');
         $samplingsQueryBuilder->where('products.id', $productId);
 
         $limit = $request->has('limit') && $request->limit < 1000 ? $request->limit : 1000;
 
-        if ($request->date)
-        {
-            $samplingsQueryBuilder->whereDate('samplings.created_at', '=', $request->date);
-            $dateFilter = true;
-        }
-        elseif ($request->exists("today"))
-        {
-            $samplingsQueryBuilder->whereDate('samplings.created_at', '=', Carbon::today()->toDateString());
-            $dateFilter = true;
-        }
-        elseif ($request->exists("yesterday"))
-        {
-            $samplingsQueryBuilder->whereDate('samplings.created_at', '=', Carbon::today()->subDay()->toDateString());
-            $dateFilter = true;
-        }
-        elseif ($request->start)
-        {
-            $samplingsQueryBuilder->whereDate('samplings.created_at', '>=', $request->start);
-            $dateFilter = true;
+        $timeFiltersApplied = $this->filterResultsInTime($samplingsQueryBuilder, $request);
 
-            if ($request->end)
-            {
-                $samplingsQueryBuilder->whereDate('samplings.created_at', '<=', $request->end);
-                $dateFilter = true;
-            }
-        }
 
-        if ( ! $dateFilter)
+        if ( ! $timeFiltersApplied)
         {
             $samplingsQueryBuilder->whereDate('samplings.created_at', '=', Carbon::today()->toDateString());
         }
@@ -94,5 +69,40 @@ class SamplingsController extends Controller
                 'samplings.created_at'
             )
         );
+    }
+
+    private function filterResultsInTime(Builder &$samplingsQueryBuilder, $request)
+    {
+        if ($request->date)
+        {
+            $samplingsQueryBuilder->whereDate('samplings.created_at', '=', $request->date);
+            return true;
+        }
+
+        if ($request->exists("today"))
+        {
+            $samplingsQueryBuilder->whereDate('samplings.created_at', '=', Carbon::today()->toDateString());
+            return true;
+        }
+
+        if ($request->exists("yesterday"))
+        {
+            $samplingsQueryBuilder->whereDate('samplings.created_at', '=', Carbon::today()->subDay()->toDateString());
+            return true;
+        }
+
+        if ($request->start)
+        {
+            $samplingsQueryBuilder->whereDate('samplings.created_at', '>=', $request->start);
+
+            if ($request->end)
+            {
+                $samplingsQueryBuilder->whereDate('samplings.created_at', '<=', $request->end);
+            }
+
+            return true;
+        }
+
+        return false;
     }
 }
